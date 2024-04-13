24ad5efbd6829cdac2e0343a6584931d61041d8b
Munazzahk <munazzah2004@gmail.com>
2024-04-13 16:29:52 +0200

    First Commit

---
+++ src/main/java/com/example/nameandpassword/NameAndPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.nameandpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameAndPasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameAndPasswordApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/nameandpassword/controllers/ValidateController.java
@@ -0,0 +1,23 @@
+package com.example.nameandpassword.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ValidateController {
+
+    @PostMapping("/validate")
+    public String validate(Model model, @RequestParam String name,
+                           @RequestParam String password) {
+
+        if (name.equals(password)) {
+            model.addAttribute("message", "Right!");
+            return "home/index";
+        } else {
+            model.addAttribute("message", "Wrong!");
+            return "home/index";
+        }
+    }
+}
